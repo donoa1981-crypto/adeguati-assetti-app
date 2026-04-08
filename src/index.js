7e025bf2cdaf57351c8370e338719ac5bf98d923
donoa1981-crypto <donoa1981@gmail.com>
2026-04-08 18:28:24 +0200

    Create src/index.js with React render code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './renderer/app';
+import './renderer/styles/main.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
